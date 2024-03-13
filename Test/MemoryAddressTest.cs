6ac16645fc446d9d3051a41800ae2d532d92b284
kuboshu83 <dot.sohkubo@gmail.com>
2024-03-13 15:11:02 +0900

    テスト追加

--- Test/MemoryAddressTest.cs
+++ Test/MemoryAddressTest.cs
@@ -40,4 +40,21 @@ public class MemoryAddressTest
         );
     }
 
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [Description("MemoryAddress同士を加算して正しいMemoryAddressができる")]
+    public void Increment_MemoryAddressを加算する(int addr1, int addr2, int expected)
+    {
+        // arrange
+        var memoryAddress1 = new MemoryAddress(new AddressValue(addr1));
+        var memoryAddress2 = new DataLength(new AddressValue(addr2));
+        // act
+        var calcuratedAddress = memoryAddress1.Increment(memoryAddress2);
+        // assert
+        Assert.Equal(
+            new MemoryAddress(new AddressValue(expected)).Value,
+            calcuratedAddress.Value
+        );
+    }
+
 }
